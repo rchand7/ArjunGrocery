b0ffb8efd5bf78858493c4549560807f1aeff2b7
rchand7 <150305815+rchand7@users.noreply.github.com>
2025-08-11 10:59:11 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ const app = express();
 
 await connectCloudinary();
 // allow multiple origins
-const allowedOrigins = ["http://localhost:5173"];
+const allowedOrigins = ["https://arjungrocery-2.onrender.com"];
 //middlewares
 app.use(cors({ origin: allowedOrigins, credentials: true }));
 app.use(cookieParser());
